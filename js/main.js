ddc38ae2e77e32e2b0465846205f5e8c0e7c8eb1
Steve Park <ghkfkd0323@gmail.com>
2020-04-04 23:28:18 -0700

    add conditional statement for centering layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 
-    // INPUT EVENT LISTENER 
+    // GET INPUT ELEMENT FOR EVENT LISTENER 
 const searchInput = document.getElementById('search');
+
+    // GALLERY CLASS TO CONTROL FLEX JUSTIFY CONTENT PROPERTY
+const gallery = document.getElementsByClassName('gallery')[0];
+gallery.style.justifyContent = "center"
 
 searchInput.addEventListener("input", e => {
     // OR
@@ -8,22 +12,24 @@ searchInput.addEventListener("input", e => {
     //    ...
     // })
 
+    // PUT THE SEARCH RESULTS TO TOP LEFT SIDE
+    gallery.style.justifyContent = "flex-start"
+
     // PARAMETER INPUT EVENT VALUE
-    let inputvalue = e.target.value
+    let inputValue = e.target.value
 
     // SELECT ALL ANCHOR TAG FROM GALLERY
     let imageAnchor = document.querySelectorAll('.photo a');
 
     // LOOPING THROUGH EACH ANCHOR TAG 'DATA-TITLE' ATTRIBUTE IN GALLERY DIV
     imageAnchor.forEach(element => {
+
         // PARAMETER ELEMENT IS WHOLE ANCHOR TAG IN HTML
-        
-        
         // GET DATA TITLE( CAPTION )  FROM ANCHOR TAG
         let title = element.getAttribute('data-title').toLowerCase();
         
         // IF TITLE DOES NOT INCLUDE INPUT VALUE SET DISPLAY NONE
-        if( !title.includes(inputvalue.toLowerCase()) ) {
+        if( !title.includes(inputValue.toLowerCase()) ) {
             element.parentElement.style.display = "none"
             
             // UNSET GROUP OF IMAGES FROM LIGHTBOX PLUGIN
@@ -34,4 +40,9 @@ searchInput.addEventListener("input", e => {
             element.parentElement.style.display = "block"
         }
     });
+
+    // IF THERE IS NO INPUT VALUES IN THE SEARCHBOX, SET GALLERY DIV TO CENTER
+    if (inputValue === "") {
+        gallery.style.justifyContent = "center"
+    }
 })
