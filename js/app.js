13ed3332ccf4092b7a63681dce947a958e19d16d
Steve Park <ghkfkd0323@gmail.com>
2020-05-30 23:47:05 -0700

    add transition

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 $(document).ready( () => {
     
     // IMAGE HOVERING 
-    $('.photo').mouseover( (e) => {
+    $('.photo').hover( (e) => {
         console.log('img hover test',e.target)
 
         // SIMPLE STYLE WORKING TEST
-        e.target.style.display = 'none'
+        e.target.style.opacity = ".5"
+        e.target.style.transition = ".3s ease-in"
+    }, (e) => {
+        e.target.style.opacity = '1'
+        e.target.style.transition = ".3s ease-in"
     });
 
 });
